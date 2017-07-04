ff994f04060afc1cd43a1bc08e5bf701c261cc66
Ross Melville <rossmelville@hotmail.com>
2017-07-04 15:03:20 +0100

    Initial commit. Bus and Person classes created.

---
+++ bus.rb
@@ -0,0 +1,8 @@
+class Bus
+
+def initailize(route_number, destination)
+  @route_number = route_number
+  @destination = destination
+end
+
+end
---
+++ passenger.rb
@@ -0,0 +1,7 @@
+class Person
+
+def initialize(name, age)
+  @name = name
+  @age = age
+
+end
