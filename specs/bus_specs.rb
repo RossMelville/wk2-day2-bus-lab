6211fe9cd20323498bd1e3b33e3c212a8214399a
Ross Melville <rossmelville@hotmail.com>
2017-07-04 15:25:26 +0100

    Succesfully added buses and drive method to bus class.

--- specs/bus_specs.rb
+++ specs/bus_specs.rb
@@ -1,6 +1,6 @@
 require('minitest/autorun')
 require_relative('../bus.rb')
-require_relative('../person.rb')
+# require_relative('../person.rb')
 
 class TestBus < MiniTest::Test
 
@@ -12,3 +12,18 @@ def setup
 
 end
 
+def test_bus_number
+  assert_equal(22, @bus1.route_number)
+end
+
+def test_bus_destination
+  assert_equal("Edinburgh Airport", @bus2.destination)
+end
+
+def test_bus_drive_feature
+  assert_equal("Brum Brum", @bus1.drive)
+end
+
+
+
+end
