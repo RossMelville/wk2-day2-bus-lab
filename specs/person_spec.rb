3159889abb1e35af894897279723ddc30a71310c
Ross Melville <rossmelville@hotmail.com>
2017-07-04 15:32:35 +0100

    Person class completed and tested that inputs are returning correct values, all test passed.

--- specs/person_spec.rb
+++ specs/person_spec.rb
@@ -3,3 +3,23 @@ require_relative('../bus.rb')
 require_relative('../person.rb')
 
 class TestPerson < MiniTest::Test
+
+  def setup
+    @person1 = Person.new("Ross", 21)
+    @person2 = Person.new("Andrew", 18)
+    @person3 = Person.new("Alex", 23)
+    @person4 = Person.new("Donald", 60)
+  end
+
+def test_person_name
+  assert_equal("Ross", @person1.name)
+  assert_equal("Donald", @person4.name)
+end
+
+def test_person_age
+  assert_equal(21, @person1.age)
+  assert_equal(23, @person3.age)
+end
+
+
+end
